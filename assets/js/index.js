1795a4b31ca85261fafaa9f67789d759edaba0bb
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-11-15 15:15:12 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+const countryOneLocalPrice = 5.71
+let countryOne = "United States" 
+let countryCurrencyOne = "USD"
+let countryOneResult = 10 * countryOneLocalPrice
+console.log("For 10 Burgers the price in :" + countryOne + " " + "is:" + " " + countryOneResult + " " + countryCurrencyOne)
+
+const countryTwoLocalPrice = 4.21
+let countryTwo = "European Union" 
+let countryCurrencyTwo = "EUR"
+let countryTwoResult = 10 * countryTwoLocalPrice
+console.log("For 10 Burgers the price in :" + countryTwo + " " + "is:" + " " + countryTwoResult + " " + countryCurrencyTwo)
+
+
+const countryThreeLocalPrice = 21.7
+let countryThree = "China" 
+let countryCurrencyThree = "CNY"
+let countryThreeResult = 10 * countryThreeLocalPrice
+console.log("For 10 Burgers the price in :" + countryThree + " " + "is:" + " " + countryThreeResult + " " + countryCurrencyThree)
+
+
+const countryFourLocalPrice = 390.0
+let countryFour = "Japan" 
+let countryCurrencyFour = "JPY"
+let countryFourResult = 10 * countryFourLocalPrice
+console.log("For 10 Burgers the price in :" + countryFour + " " + "is:" + " " + countryFourResult + " " + countryCurrencyFour)
+
+
+const countryFiveLocalPrice = 190.0
+let countryFive = "India" 
+let countryCurrencyFive = "INR"
+let countryFiveResult = 10 * countryFiveLocalPrice
+console.log("For 10 Burgers the price in :" + countryFive + " " + "is:" + " " + countryFiveResult + " " + countryCurrencyFive)
+
+
+const countrySixLocalPrice = 3.39
+let countrySix = "United Kingdom" 
+let countryCurrencySix = "GBP"
+let countrySixResult = 10 * countrySixLocalPrice
+console.log("For 10 Burgers the price in :" + countrySix + " " + "is:" + " " + countrySixResult + " " + countryCurrencySix)
+
+console.log("Is : " + " " + countryFourResult + ">" +  countrySixResult + " " + "???")
+console.log(countryFourResult > countrySixResult)
